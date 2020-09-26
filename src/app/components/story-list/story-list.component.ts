6d71e834a17289676f1366662b1bd7d4670a92ae
Marcel Riera <mrier011@fiu.edu>
2020-09-25 23:55:05 -0400

    ST-0007 fix story list sort order

--- src/app/components/story-list/story-list.component.ts
+++ src/app/components/story-list/story-list.component.ts
@@ -1,24 +1,63 @@
-import {Component, Input, OnInit, OnChanges} from '@angular/core';
+import {Component, Input, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 import {StoryItem} from '../../models/types';
 import {select, Store} from '@ngrx/store';
 import {StorySelectors} from '../../store/selectors';
-import {map, tap, withLatestFrom} from 'rxjs/operators';
+import {concatMap, map, switchMap, tap, withLatestFrom} from 'rxjs/operators';
 import {StoryActions} from '../../store/actions';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
 import {CdkDragDrop} from '@angular/cdk/drag-drop';
+import {selectItemId} from '../../store/reducers/story.reducer';
 
 @Component({
   selector: 'app-story-list',
   templateUrl: './story-list.component.html',
   styleUrls: ['./story-list.component.scss']
 })
-export class StoryListComponent implements OnInit {
+export class StoryListComponent implements OnInit, OnChanges {
 
   @Input() public readonly storyIDs: number[];
   @Input() public readonly showStatus = true;
 
-  constructor(private readonly store: Store) {}
+  public readonly sortedIDs$: Observable<number[]>;
+  private readonly inputIDs$: BehaviorSubject<number[]>;
 
-  ngOnInit() {}
+  constructor(private readonly store: Store) {
+    this.inputIDs$ = new BehaviorSubject<number[]>([]);
+
+    // TODO: THIS IS TRASH >.> refactor into something better
+    this.sortedIDs$ = this.inputIDs$.pipe(
+      switchMap(ids => this.store.pipe(
+        select(StorySelectors.selectAllStoryItems),
+        map(stories => {
+          const set = new Set<number>(ids);
+          const sorted = [];
+
+          stories.forEach(story => {
+            const id = selectItemId(story);
+            if (set.has(id)) {
+              sorted.push(id);
+              set.delete(id);
+            }
+          });
+
+          for (const id of set) {
+            sorted.push(id);
+          }
+          return sorted;
+        })
+      ))
+    );
+
+  }
+
+  ngOnInit() {
+    this.inputIDs$.next(this.storyIDs);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.storyIDs && changes.storyIDs.currentValue) {
+      this.inputIDs$.next(changes.storyIDs.currentValue);
+    }
+  }
 
 }
